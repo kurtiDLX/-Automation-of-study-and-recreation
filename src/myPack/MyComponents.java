61b07b12bee80d33b10157dba80ed73f2365949a
Mark <marikgames12@gmail.com>
2023-07-15 13:08:37 +0300

    added: - opening my IDE after clicking the button - opening a file with all projects on the C drive - discovering Spotify, I love listening to music when I work out :)

--- src/myPack/MyComponents.java
+++ src/myPack/MyComponents.java
@@ -2,6 +2,7 @@ package myPack;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 import java.net.URI;
 
 public class MyComponents extends JFrame {
@@ -17,11 +18,17 @@ public class MyComponents extends JFrame {
         add(buttonProgramming);
         add(buttonRest);
         buttonProgramming.addActionListener(e -> {
+            String ideRun = "F:/IntelliJ IDEA Community Edition 2022.3.2/bin/idea64.exe";
+            String ideProjectsFile = "C://Users//Марк//IdeaProjects";
+            String spotifyRun = "C:/Users/Марк/AppData/Roaming/Spotify/Spotify.exe";
              try {
                Desktop.getDesktop().browse(new URI("https://github.com/"));
                Desktop.getDesktop().browse(new URI("https://elearn.epam.com/courses/course-v1:RD_CEE+Fundamentals+0922/courseware/845eb83b5db047dc801b6ace9482ebf6/f271092003984b1b937a883662dab207/2?tpa_hint=oa2-prod-elearn-iam"));
                Desktop.getDesktop().browse(new URI("https://login.neurodub.ai/u/login?state=hKFo2SB5aTloV2xCNHJQVmtzc3U0WFE2RVZRMXk0TlNubUZCeaFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIDJkNy1manJfdlFtZWhXbmpwNGsza0JqUHNnT292QXBOo2NpZNkgOVcwRGl2NnAyNGcwWUhnWVMzVUZ5SGltVWZQV1FxRGk"));
                Desktop.getDesktop().browse(new URI("https://www.youtube.com/"));
+               Desktop.getDesktop().open(new File(ideRun));
+               Desktop.getDesktop().open(new File(ideProjectsFile));
+               Desktop.getDesktop().open(new File(spotifyRun));
                System.exit(0);
              }catch (Exception ex){
                  ex.printStackTrace();
