7ccb37b15ce258d814dbcab426778a82eb1d325a
Mark <marikgames12@gmail.com>
2023-07-15 13:35:09 +0300

    added: - changed the size of the icon

--- src/myPack/MainWindow.java
+++ src/myPack/MainWindow.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class MainWindow {
     public static void main(String[] args) {
         MyComponents myProgram = new MyComponents();
-        myProgram.setSize(400, 300);
+        myProgram.setSize(400, 345);
         myProgram.setLocation(450, 300);
         myProgram.getContentPane().setBackground(Color.PINK);
         myProgram.setDefaultCloseOperation(MyComponents.EXIT_ON_CLOSE);
