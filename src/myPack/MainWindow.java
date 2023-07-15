741058e0c0c62999951e172ee2ec2d6b7be3fcda
Mark <marikgames12@gmail.com>
2023-07-15 11:56:11 +0300

    now after the user selects an option, the app will automatically close

--- src/myPack/MainWindow.java
+++ src/myPack/MainWindow.java
@@ -3,9 +3,9 @@ package myPack;
 public class MainWindow {
     public static void main(String[] args) {
         MyComponents myProgram = new MyComponents();
-        myProgram.setVisible(true);
-        myProgram.setDefaultCloseOperation(MyComponents.EXIT_ON_CLOSE);
         myProgram.setSize(400, 300);
         myProgram.setLocation(450, 300);
+        myProgram.setDefaultCloseOperation(MyComponents.EXIT_ON_CLOSE);
+        myProgram.setVisible(true);
     }
 }
--- src/myPack/MyComponents.java
+++ src/myPack/MyComponents.java
@@ -6,23 +6,24 @@ import java.net.URI;
 
 public class MyComponents extends JFrame {
     private JLabel textOne;
-    private JButton buttonProgramming, buttonVacation;
+    private JButton buttonProgramming, buttonRest;
     public MyComponents(){
         super("AOSAR");
         setLayout(new FlowLayout());
         textOne = new JLabel("Choose:");
         buttonProgramming = new JButton("programming");
-        buttonVacation = new JButton("vacation");
+        buttonRest = new JButton("rest");
         add(textOne);
         add(buttonProgramming);
-        add(buttonVacation);
+        add(buttonRest);
         buttonProgramming.addActionListener(e -> {
              try {
                Desktop.getDesktop().browse(new URI("https://chat.openai.com/"));
                Desktop.getDesktop().browse(new URI("https://github.com/"));
                Desktop.getDesktop().browse(new URI("https://elearn.epam.com/courses/course-v1:RD_CEE+Fundamentals+0922/courseware/845eb83b5db047dc801b6ace9482ebf6/f271092003984b1b937a883662dab207/2?tpa_hint=oa2-prod-elearn-iam"));
                Desktop.getDesktop().browse(new URI("https://login.neurodub.ai/u/login?state=hKFo2SB5aTloV2xCNHJQVmtzc3U0WFE2RVZRMXk0TlNubUZCeaFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIDJkNy1manJfdlFtZWhXbmpwNGsza0JqUHNnT292QXBOo2NpZNkgOVcwRGl2NnAyNGcwWUhnWVMzVUZ5SGltVWZQV1FxRGk"));
-               Desktop.getDesktop().browse(new URI("https://www.youtube.com/ "));
+               Desktop.getDesktop().browse(new URI("https://www.youtube.com/"));
+               System.exit(0);
              }catch (Exception ex){
                  ex.printStackTrace();
              }
